df8c8d272565dc35d78dff1fa1ed3452baf9a1d5
ffeN1507 <106774019+ffeN1507@users.noreply.github.com>
2022-06-10 01:03:11 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,23 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+            <div className="centered">
+                <img 
+                className="profile_image"
+                // Image goes here
+                alt="Profile Pic"
+                ></img>
+            </div>
+        </div>
+            <h1 style="color: lightblue">
+                Roberto<br />Sanchez
+            </h1>
+        <p>Hello, my name is Roberto Sanchez (although most people just call me 
+        Rob). I am a Computer Engineering major at the Universit of Houston-Clear 
+        Lake and I plan to graduate in the Fall of 2023. Some of my interests 
+        include playing volleyball, art, and playing video games.
+        </p> 
       </div>
     )
   }
